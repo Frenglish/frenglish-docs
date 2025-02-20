6230d51a3b4a00150b60f3011f35ea635e8b2fdf
PhABC <ph.castonguay@gmail.com>
2025-02-20 14:45:53 -0500

    Add new logo + delete build related files

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/vivian/main/software/Frenglish-organization/frenglish-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/vivian/main/software/Frenglish-organization/frenglish-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/vivian/main/software/Frenglish-organization/frenglish-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/vivian/main/software/Frenglish-organization/frenglish-docs/src/css/custom.css"),
-  require("/Users/vivian/main/software/Frenglish-organization/frenglish-docs/src/clientModules/i18nLoader.js"),
-];
